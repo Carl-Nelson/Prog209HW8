82beecc5ccfbbd30af42260eab74a03d106a0526
Carl-Nelson <50152324+Carl-Nelson@users.noreply.github.com>
2020-03-13 21:00:07 -0700

    Added a bunch of stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let userArray = [];
 let gameArray = [];
 let reviewArray = [];
+let gameCollection = [];
 let currentUser = "You are not logged in";
 
 var GameObject = function(name, year, genre) {
@@ -17,11 +18,10 @@ var ReviewObject = function(gameName, review, rating, user) {
 }
 
 // define a constructor to create user objects
+//not really being used for anything worthwhile yet
 var userObject = function (Name) {
   this.name = Name;
 }
-
-
 
 function createList()
 {
@@ -29,44 +29,144 @@ function createList()
   var divUserlist = $("#divUserlist");
   //and this next line doesn't work unless I use plain ole html to grab the div
   document.getElementById("divUserlist").innerHTML = "";// remove old data
-  //conclusion? I hate jquery mobile. jquery is fine. jquery mobile? there's probably a reason it was abandoned years ago.
 
-  gameArray.forEach(function (element) {   // use handy array forEach method
-    //did you know javascript has string interpolation? I didn't! They call 'em template strings apparently
-    let content = `<div data-role='collapsible' data-theme='b' data-content-theme='a' data-parm='${element.name}'><h3>${element.name}</h3>
+  gameArray.forEach(function (element) {
+    
+    let content = `<div data-role='collapsible' data-theme='b' data-content-theme='a' class='oneGame'><h3>${element.name}</h3>
                   <p>Year:${element.year}<br>Genre:${element.genre}<br>
-                  <button data-role='button'class="addButton">Add to library</button>&nbsp<button data-role='button' class="detailButton">View Details</button></p></div>`;
+                  <button data-role='button' class="addButton" gameName=${element.name}>Add to library</button>&nbsp
+                  <button data-role='button' data-parm='${element.name}' class="detailButton">View Details</button></p></div>`;
                   //the button styles worked, and now they don't. Why? Who knows!
       divUserlist.append(content).collapsibleset("refresh");//add the new collapsible widget to the set, then refresh the set
     });
 
-  // var ul = document.createElement('div');
-  // ul.setAttribute("data-role","collapsible-set");
-  // ul.setAttribute("data-theme","b");
-  // ul.setAttribute("data-content-theme","a");
-  // userArray.forEach(function (element,) {   // use handy array forEach method
-  //   var li = document.createElement('div');
-  //   ul.setAttribute("data-role","collapsible");
-  //   ul.setAttribute("data-theme","b");
-  //   ul.setAttribute("data-content-theme","a");
-  //   li.innerHTML = "<a data-transition='pop' class='onePlayer' data-parm=" + element.name + "  href='#page3'>Jump </a> " + element.name;
-  //   ul.append(li);
-  // });
-  // divUserlist.append(ul);
-
   //set up an event for each new li item, if user clicks any, it writes >>that<< items data-parm into the hidden html 
-  var classname = document.getElementsByClassName("oneGame");
+  var classname = document.getElementsByClassName("detailButton");
   Array.from(classname).forEach(function (element) {
-        element.addEventListener('click', function(){
-            var parm = this.getAttribute("data-parm");  // passing in the record.Id
-            console.log(parm);
-            //do something here with parameter on  pickbet page
-            document.getElementById("IDparmHere").innerHTML = parm;
-            document.location.href = "index.html#details";
-        });
+    element.addEventListener('click', function(){
+      var parm = this.getAttribute("data-parm");  // passing in the record.Id
+      console.log(parm);
+      //do something here with parameter on pickbet page
+      document.getElementById("IDparmHere").innerHTML = parm;
+      $.mobile.navigate("#details");
     });
+  });
+  var classname = document.getElementsByClassName("addButton");
+  Array.from(classname).forEach(function (element) {
+    element.addEventListener('click', function(){
+      let gameName = this.getAttribute("gameName");
+      //make sure the game isn't already in the collection
+      for (let i = 0; i < gameCollection.length; i++) {
+        if (gameCollection[i].name === gameName) {
+          this.innerHTML = "Already in library";
+          return;
+        }
+      }
+      //find the proper game object to add to the collection
+      for (let i = 0; i < gameArray.length; i++) {
+        if (gameName === gameArray[i].name) {
+          gameCollection.push(gameArray[i]);
+          this.innerHTML = "Game added";
+        }
+      }
+    });
+  });
   console.log(gameArray);
-};
+}
+
+function compareTitle(a, b) {
+  // Use toUpperCase() to ignore character casing
+  const gameA = a.name.toUpperCase();
+  const gameB = b.name.toUpperCase();
+
+  let comparison = 0;
+  if (gameA > gameB) {
+    comparison = 1;
+  } else if (gameA < gameB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+
+function compareGenre(a, b) {
+  // Use toUpperCase() to ignore character casing
+  const gameA = a.genre.toUpperCase();
+  const gameB = b.genre.toUpperCase();
+
+  let comparison = 0;
+  if (gameA > gameB) {
+    comparison = 1;
+  } else if (gameA < gameB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+//this... might be unnecessary
+function compareYear(a, b) {
+  const gameA = a.year;
+  const gameB = b.year;
+
+  if (gameA > gameB) {
+    return 1;
+  } else if (gameA < gameB) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+function displayCollection() {
+    //oddly enough the jquery mobile collapsible set stuff below doesn't work unless I use jquery to get the div
+    var collection = $("#collection");
+    //and this next line doesn't work unless I use plain ole html to grab the div
+    if (gameCollection.length === 0) {
+      document.getElementById("collection").innerHTML = "";
+      collection.append("<p>Your library is empty. Add some games!</p>");
+    }
+    else {
+      document.getElementById("collection").innerHTML = "";// remove old data
+  
+      gameCollection.forEach(function (element) {
+        
+        let content = `<div data-role='collapsible' data-theme='b' data-content-theme='a' class='oneGame'><h3>${element.name}</h3>
+                      <p>Year:${element.year}<br>Genre:${element.genre}<br>
+                      <button data-role='button' class="removeButton" gameName=${element.name}>Remove from library</button>&nbsp
+                      <button data-role='button' data-parm='${element.name}' class="detailButton">View Details</button></p></div>`;
+                      //the button styles worked, and now they don't. Why? Who knows!
+          collection.append(content).collapsibleset("refresh");//add the new collapsible widget to the set, then refresh the set
+        });
+    
+      //set up an event for each new li item, if user clicks any, it writes >>that<< items data-parm into the hidden html 
+      var classname = document.getElementsByClassName("detailButton");
+      Array.from(classname).forEach(function (element) {
+        element.addEventListener('click', function(){
+          var parm = this.getAttribute("data-parm");  // passing in the record.Id
+          console.log(parm);
+          //do something here with parameter on pickbet page
+          document.getElementById("IDparmHere").innerHTML = parm;
+          $.mobile.navigate("#details");
+        });
+      });
+      console.log(gameCollection);
+
+      var classname = document.getElementsByClassName("removeButton");
+      Array.from(classname).forEach(function (element) {
+        element.addEventListener('click', function(){
+          let gameName = this.getAttribute("gameName");
+          //find the game to remove
+          for (let i = 0; i < gameCollection.length; i++) {
+            if (gameCollection[i].name === gameName) {
+              this.innerHTML = "Game removed";
+              //this removes the element and changes the length of the array
+              gameCollection.splice(i,1);
+              displayCollection();
+            }
+          }
+        });
+      });
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -86,11 +186,63 @@ document.addEventListener("DOMContentLoaded", function () {
     gameArray.push(new GameObject("game6", 2002, "First Person Shooter"));
     console.log(userArray);
 
-    // createList();
+    reviewArray.push(new ReviewObject("game1","This game sucks.", 1, "firstPerson"));
+    reviewArray.push(new ReviewObject("game2","Game of the year, every year.", 5, "secondPerson"))
+
+    //so that the home page isn't empty on first load
+    displayCollection();
+
+    $(document).on("pagebeforeshow","#home", function(event){
+      displayCollection();
+    });
 
     $(document).on("pagebeforeshow", "#page1", function (event) {   // have to use jQuery 
-    // document.getElementById("IDparmHere").innerHTML = "";
+      document.getElementById("IDparmHere").innerHTML = "";
       createList();
     });
 
+    $(document).on("pagebeforeshow", "#details", function (event) {   // have to use jQuery 
+      let localTitle = document.getElementById("IDparmHere").innerHTML;
+      for(let i=0; i < gameArray.length; i++) {   
+        if(gameArray[i].name == localTitle){
+          //document.getElementById("oneName").innerHTML = gameArray[i].name;
+          document.getElementById("oneYear").innerHTML = "Year released: " + gameArray[i].year;
+          document.getElementById("oneGenre").innerHTML = "Genre: " + gameArray[i].genre;
+        }
+      }
+      for(let i=0; i < reviewArray.length; i++) {
+        if(reviewArray[i].gameName == localTitle) {
+
+        }
+      }
+   });
+
+   document.getElementById("buttonSortTitle").addEventListener("click", function () {
+    gameCollection = gameCollection.sort(compareTitle);
+    displayCollection();
+  });
+
+  document.getElementById("buttonSortGenre").addEventListener("click", function () {
+    gameCollection = gameCollection.sort(compareGenre);
+    displayCollection();
+  });
+
+  document.getElementById("buttonSortYear").addEventListener("click", function() {
+    gameCollection = gameCollection.sort(compareYear);
+    displayCollection();
+  })
+  document.getElementById("buttonSortAllTitle").addEventListener("click", function () {
+    gameArray = gameArray.sort(compareTitle);
+    createList();
+  });
+
+  document.getElementById("buttonSortAllGenre").addEventListener("click", function () {
+    gameArray = gameArray.sort(compareGenre);
+    createList();
+  });
+
+  document.getElementById("buttonSortAllYear").addEventListener("click", function() {
+    gameArray = gameArray.sort(compareYear);
+    createList();
+  })
 });
